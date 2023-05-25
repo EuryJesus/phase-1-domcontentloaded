708c22618c31c1dd591bca06bb2d4382df55888a
EuryJesus <Eury0218@hotmail.com>
2023-05-25 10:45:04 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded',() => {
+    console.log('The DOM has Loaded')
+    let HZ = document.querySelector('p')
+    HZ.textContent = 'This is really cool!'
+})
+    console.log('Before DOM Loads')
+    console.log(document.querySelector('p'))
